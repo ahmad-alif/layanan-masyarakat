6b6ac39ae72cc2089dc9694668c1fdd7738f4af2
ahmad-alif <61868297+ahmad-alif@users.noreply.github.com>
2020-03-06 16:57:21 +0700

    Config to database

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+
+$dbHost = 'localhost';
+$dbUsername = 'root';
+$dbPassword = '';
+$dbName = 'layananmasyarakat';
+
+$mysqli = mysqli_connect($dbHost, $dbUsername, $dbPassword, $dbName);
+
+?>
